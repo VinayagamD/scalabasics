a63896064d43c00d7291f62ec04011a29b2ad1a2
VinayagamD <vinayganeshdxj@gmail.com>
2019-04-21 23:45:42 +0530

    Closures

---
+++ src/main/scala/demo/Closures.scala
@@ -0,0 +1,16 @@
+package demo
+
+object Closures {
+
+  val number = 10
+//  val add = (x: Int) => x + 10
+  val add = (x: Int) => {
+    x+number
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(add(20))
+    println(number)
+  }
+
+}
